d35e641562c5c99b59f24f222143c03abd7b3200
ozngnr <ozngnr@gmail.com>
2023-02-04 09:37:02 +0000

    updated _document.js to test styled components

--- pages/_document.js
+++ pages/_document.js
@@ -1,48 +1,80 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
-export default class MyDocument extends Document {
+// export default class MyDocument extends Document {
+//   static async getInitialProps(ctx) {
+//     const sheet = new ServerStyleSheet();
+//     const originalRenderPage = ctx.renderPage;
+
+//     try {
+//       ctx.renderPage = () =>
+//         originalRenderPage({
+//           enhanceApp: (App) => (props) =>
+//             sheet.collectStyles(<App {...props} />),
+//         });
+
+//       const initialProps = await Document.getInitialProps(ctx);
+//       return {
+//         ...initialProps,
+//         styles: [initialProps.styles, sheet.getStyleElement()],
+//       };
+//     } finally {
+//       sheet.seal();
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <Html lang="en">
+//         <Head>
+//           <meta
+//             name="description"
+//             content="Highly passionate and creative Front End Developer, based in London. Specialising in React, Gatsby and Next.js"
+//           />
+//           <link rel="icon" href="/favicon.ico" />
+//           <link rel="preconnect" href="https://fonts.googleapis.com" />
+//           <link
+//             rel="preconnect"
+//             href="https://fonts.gstatic.com"
+//             crossOrigin="true"
+//           />
+//           <link
+//             href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap"
+//             rel="stylesheet"
+//           ></link>
+//         </Head>
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     );
+//   }
+// }
+
+class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
+    // Run the React rendering logic synchronously
+    ctx.renderPage = () =>
+      originalRenderPage({
+        // Useful for wrapping the whole react tree
+        enhanceApp: (App) => App,
+        // Useful for wrapping in a per-page basis
+        enhanceComponent: (Component) => Component,
+      });
 
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: [initialProps.styles, sheet.getStyleElement()],
-      };
-    } finally {
-      sheet.seal();
-    }
+    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
   }
 
   render() {
     return (
-      <Html lang="en">
-        <Head>
-          <meta
-            name="description"
-            content="Highly passionate and creative Front End Developer, based in London. Specialising in React, Gatsby and Next.js"
-          />
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap"
-            rel="stylesheet"
-          ></link>
-        </Head>
+      <Html>
+        <Head />
         <body>
           <Main />
           <NextScript />
@@ -51,3 +83,5 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+export default MyDocument;
