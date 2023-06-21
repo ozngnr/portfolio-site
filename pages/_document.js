dcdc72409b7eaf42d406c1bda2fce61032d636bf
ozngnr <ozngnr@gmail.com>
2023-06-21 19:54:14 +0100

    GTM script update

--- pages/_document.js
+++ pages/_document.js
@@ -28,35 +28,31 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/* <!-- Google tag (gtag.js) --> */}
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-ECQVZ0CX9Z"
+          ></Script>
+          <script>
+            {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date());
+
+            gtag('config', 'G-ECQVZ0CX9Z');`}
+          </script>
           <meta
             name="description"
             content="Highly passionate and creative Front End Developer, based in London. Specialising in React js"
           />
           <link rel="icon" href="/favicon.ico" />
           {/* <!-- Google Tag Manager --> */}
-          <Script
-            id="google-tag-manager"
-            strategy="afterInteractive"
-          >{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-KTZWCW3');`}</Script>
+          })(window,document,'script','dataLayer','GTM-KTZWCW3');`}</script>
           {/* <!-- End Google Tag Manager --> */}
         </Head>
-
-        {/* <!-- Google tag (gtag.js) --> */}
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-ECQVZ0CX9Z"
-        ></Script>
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments)}
-            gtag('js', new Date());
-
-            gtag('config', 'G-ECQVZ0CX9Z');`}
-        </Script>
         <body>
           {/* <!-- Google Tag Manager (noscript) --> */}
           <noscript
