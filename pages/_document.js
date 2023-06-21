cc37897d0c1ec52e32cde78ba602054eaa13a547
ozngnr <ozngnr@gmail.com>
2023-06-21 09:31:15 +0100

    dangerously set inner html on noscript tag

--- pages/_document.js
+++ pages/_document.js
@@ -59,14 +59,11 @@ export default class MyDocument extends Document {
         </Head>
         <body>
           {/* <!-- Google Tag Manager (noscript) --> */}
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-KTZWCW3"
-              height="0"
-              width="0"
-              style="display:none;visibility:hidden"
-            ></iframe>
-          </noscript>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KTZWCW3" height="0" width="0" style="display:none;visibility:hidden"/>`,
+            }}
+          />
           {/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
