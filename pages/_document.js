5b2a11b5f98a38284cbeb84cae4736aef5c9dd85
ozngnr <ozngnr@gmail.com>
2023-06-21 09:18:05 +0100

    installed google tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -33,9 +33,22 @@ export default class MyDocument extends Document {
             content="Highly passionate and creative Front End Developer, based in London. Specialising in React js"
           />
           <link rel="icon" href="/favicon.ico" />
+          {/* <!-- Google Tag Manager --> */}
+          <Script
+            id="google-tag-manager"
+            strategy="afterInteractive"
+          >{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KTZWCW3');`}</Script>
+          {/* <!-- End Google Tag Manager --> */}
 
           {/* <!-- Google tag (gtag.js) --> */}
-          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-ECQVZ0CX9Z"></Script>
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-ECQVZ0CX9Z"
+          ></Script>
           <Script id="google-analytics" strategy="afterInteractive">
             {`window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments)}
@@ -45,6 +58,16 @@ export default class MyDocument extends Document {
           </Script>
         </Head>
         <body>
+          {/* <!-- Google Tag Manager (noscript) --> */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-KTZWCW3"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            ></iframe>
+          </noscript>
+          {/* <!-- End Google Tag Manager (noscript) --> */}
           <Main />
           <NextScript />
         </body>
