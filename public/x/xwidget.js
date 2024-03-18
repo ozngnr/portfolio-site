5065227a691d62096ea8cda55ee92767ffde25e2
Ozan Guner <ozngnr@gmail.com>
2024-03-18 19:02:55 +0000

    update

--- public/x/xwidget.js
+++ public/x/xwidget.js
@@ -15,15 +15,16 @@ function initSupabase(callback) {
   }
   document.head.appendChild(script)
 }
-initSupabase(function () {
-  // Your code that depends on Supabase goes here. For example:
-  const SUPABASE_URL = 'https://npwoitbpqbsundoyaplm.supabase.co'
-  const SUPABASE_ANON_KEY =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wd29pdGJwcWJzdW5kb3lhcGxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcyMjUyNTEsImV4cCI6MjAyMjgwMTI1MX0.tP391KWyjd6uRTqWY8ow8AQCdzSBahOZ3YVOSD1hrCY'
-  const supaClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
-})
 
 const xWidget = ({ workspace_id }) => {
+  let supaClient
+  initSupabase(function () {
+    // Your code that depends on Supabase goes here. For example:
+    const SUPABASE_URL = 'https://npwoitbpqbsundoyaplm.supabase.co'
+    const SUPABASE_ANON_KEY =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wd29pdGJwcWJzdW5kb3lhcGxtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcyMjUyNTEsImV4cCI6MjAyMjgwMTI1MX0.tP391KWyjd6uRTqWY8ow8AQCdzSBahOZ3YVOSD1hrCY'
+    supaClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+  })
   // Function to get data
   const getDataByIdAndTable = (id, table) => {
     return supaClient.from(table).select().eq('id', id)
@@ -151,7 +152,7 @@ const xWidget = ({ workspace_id }) => {
     }
   }
 
-  const subscription = supaClient
+  supaClient
     .channel('workspaces')
     .on(
       'postgres_changes',
